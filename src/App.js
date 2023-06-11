597edbfa79a8e44839d2ff9125f81ee4c7c9dd0d
Matheus Castro <mate.cas37@gmail.com>
2023-06-11 12:38:21 -0300

    estilizando visual da calculadora.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-function App() {
+import Input from './components/input';
+import Button from './components/button';
+
+import {Container, Content, Row, Column} from './styles';
+
+const App = () => {
   return (
-    <div className="App">
-      OLÁ
-    </div>
+    <Container>
+      <Content>
+        <Input />
+        <Row>
+          <Button label="7"/>
+          <Button label="8"/>
+          <Button label="9"/>
+          <Button label="-"/>
+        </Row>
+
+        <Row>
+          <Button label="4"/>
+          <Button label="5"/>
+          <Button label="6"/>
+          <Button label="+"/>
+        </Row>
+
+        <Row>
+          <Button label="1"/>
+          <Button label="2"/>
+          <Button label="3"/>
+          <Button label="="/>
+        </Row>
+      </Content>
+    </Container>
   );
 }
 
